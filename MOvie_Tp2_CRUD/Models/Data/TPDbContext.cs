5a5e8e7f65232ee595fa1911955bd2b949e728a1
AnhNguyen <1904518@cegepmontpetit.ca>
2023-08-24 14:52:00 -0400

    FCT Mettre en place i18n, Traduire le contenu

---
+++ MOvie_Tp2_CRUD/Models/Data/TPDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TP2.Models.Data
+{
+    public class TPDbContext:DbContext
+    {
+        public TPDbContext(DbContextOptions<TPDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Enfant> Enfants { get; set; }
+        public DbSet<Parent> Parents { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.SetEntityRelationships();
+            modelBuilder.GenerateData();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
